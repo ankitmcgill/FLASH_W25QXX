b21684e7ffab379640843ebc87d3194bf66a7e34
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2017-03-27 11:42:23 +0530

    added basic header and C file. updated gitignore file

--- FLASH_W25QXX.c
+++ FLASH_W25QXX.c
@@ -1,1 +1,10 @@
-temp
+/*************************************************************************
+* WINBOND W25QXX FLASH CHIP LIBRARY
+*
+* MARCH 27 2017
+* ANKIT BHATNAGAR
+* ANKIT.BHATNAGARINDIA@GMAIL.COM
+*
+* ***********************************************************************/
+
+#include "FLASH_W25QXX.h"
--- FLASH_W25QXX.h
+++ FLASH_W25QXX.h
@@ -1,1 +1,15 @@
-temp
+/*************************************************************************
+* WINBOND W25QXX FLASH CHIP LIBRARY
+*
+* MARCH 27 2017
+* ANKIT BHATNAGAR
+* ANKIT.BHATNAGARINDIA@GMAIL.COM
+*
+* ***********************************************************************/
+
+#ifndef _FLASH_W25QXX_
+#define	_FLASH_W25QXX_
+
+#include <stdint.h>
+
+#endif
